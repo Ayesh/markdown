5ece475ddeb769da170d37d979fa0e49c1ba74b8
Ayesh Karunaratne <ayesh@aye.sh>
2023-12-28 13:48:49 +0700

    Update tests

--- tests/TestParsedown.php
+++ tests/TestParsedown.php
@@ -5,7 +5,7 @@ namespace Ayesh\Markdown\Tests;
 use Ayesh\Markdown\Markdown;
 
 class TestParsedown extends Markdown {
-    public function getTextLevelElements() {
-        return $this->textLevelElements;
+    public function getTextLevelElements(): array {
+        return static::textLevelElements;
     }
 }
